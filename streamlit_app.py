c740c3937c78f9ba10ecaf59657bd458405db1b6
poiu1004 <129647907+poiu1004@users.noreply.github.com>
2025-05-14 09:48:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-st.markdown("""
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="manifest" href="/manifest.json">
-<meta name="theme-color" content="#FE4949">
-""", unsafe_allow_html=True)
-
 import streamlit as st
 
 # 1) 무조건 최상단에 한 번만!
